f91f7658237f65eccf778488ff3d40901fed762b
민경 <alsrud9316@naver.com>
2022-11-18 15:02:51 +0900

    listup&showgraph

--- ptp-visualizition.py
+++ ptp-visualizition.py
@@ -6,63 +6,93 @@ app = Flask(__name__)
 datetime= []
 offset = []
 
+def getlist():
+    db = pymysql.connect(host='',
+                         user='',
+                         password='',
+                         db='',
+                         charset='utf8',
+                         cursorclass=pymysql.cursors.DictCursor)
+
+    # database 사용하기 위한 cursor 세팅
+    cursor = db.cursor()
+
+    selectnodelist = """select * from nodelist """
+
+    # Sql query실행
+    cursor.execute(selectnodelist)
+
+    # 실행결과 가져오기
+    result = cursor.fetchall()
+
+    db.close()
+    return result
+
 @app.route('/')
 def index():
-    return render_template('main.html')
+    # nodelist=[]
+    # db = pymysql.connect(host='',
+    #                      user='',
+    #                      password='',
+    #                      db='',
+    #                      charset='utf8',
+    #                      cursorclass=pymysql.cursors.DictCursor)
+    #
+    # # database 사용하기 위한 cursor 세팅
+    # cursor = db.cursor()
+    #
+    # selectnodelist="""select * from nodelist """
+    #
+    # # Sql query실행
+    # cursor.execute(selectnodelist)
+    #
+    # # 실행결과 가져오기
+    # result = cursor.fetchall()
+    #
+    # db.close()
+    # print(result)
+    return render_template('main.html', nodelist=getlist())
+global date
+date=''
+@app.route('/show/', methods=['POST'])
+def show():
+    global date
+    data = request.values
+    date=data['date']
+    return render_template('main.html', nodelist=getlist())
 
-# @app.route('/node/<int:id>/', methods=['GET', 'POST'])
-# def update(id):
-#     if request.method =='GET':
-#         datetime=''
-#         offset=''
-#
-#         db = pymysql.connect(host='',
-#                              user='',
-#                              password='',
-#                              db='',
-#                              charset='utf8',
-#                              cursorclass=pymysql.cursors.DictCursor)
-#
-#         # database 사용하기 위한 cursor 세팅
-#         cursor = db.cursor()
-#
-#         select1 = """SELECT * FROM node1"""
-#         select2 = """SELECT * FROM node2"""
-#         select3 = """SELECT * FROM node3"""
-#         selecttemp = """SELECT * FROM temp"""
-#
-#         # Sql query실행
-#         cursor.execute(selecttemp)
-#
-#         # 실행결과 가져오기
-#         result = cursor.fetchall()
-#
-#         db.close()
-#
-#         for i in result:
-#             datetime.append(result[i]['datetime'])
-#             offset.append(result[i]['offset'])
-#
-#
-#         content = f'''
-#             <form action="/update/{id}" method="POST">
-#                 <p><input type="text" name ="title" placeholder="title" value="{title}"></p>
-#                 <p><textarea name="body" placeholder="body">{body}</textarea></p>
-#                 <p><input type="submit" value="update"></p>
-#             </form>
-#             '''
-#         return template(getContents(), content)
-#     elif request.method =='POST':
-#         global nextId
-#         title = request.form['title']
-#         body = request.form ['body']
-#         for topic in topics:
-#             if id == topic['id']:
-#                 topic['title']=title
-#                 topic['body']=body
-#                 break
-#         url = '/read/'+str(id)+'/'
-#         return redirect(url)
+@app.route('/<int:id>/')
+def update(id):
+    global date
+    datetime=[]
+    offset=[]
+
+    db = pymysql.connect(host='',
+                         user='',
+                         password='',
+                         db='',
+                         charset='utf8',
+                         cursorclass=pymysql.cursors.DictCursor)
+
+    # database 사용하기 위한 cursor 세팅
+    cursor = db.cursor()
+
+    select = f"""SELECT * FROM node{id}"""
+    selecttemp="""SELECT * FROM temp"""
+
+    # Sql query실행
+    cursor.execute(selecttemp)
+
+    # 실행결과 가져오기
+    result = cursor.fetchall()
+
+    db.close()
+
+    for i in result:
+        datetime.append(i['datetime'])
+        offset.append(i['offset'])
+
+    return render_template('main.html', nodelist=getlist(), id=id, datetime=datetime, offset=offset)
 
 app.run(debug=True)
 
