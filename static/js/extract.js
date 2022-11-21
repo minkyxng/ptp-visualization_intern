dedc3f1aec29a089254a78a5856ed36cafb3bc28
민경 <alsrud9316@naver.com>
2022-11-21 20:34:54 +0900

    delete comment
    주석삭제

--- static/js/extract.js
+++ static/js/extract.js
@@ -2,8 +2,6 @@ window.onload=function(){
     var today = new Date();
     var yesterday = new Date(today.setDate(today.getDate()-1)).toISOString().substring(0, 10)
     document.getElementById('date').setAttribute('max',yesterday);
-//    document.getElementById('date').value = new Date(today.setDate(today.getDate()-1)).toISOString().substring(0, 10);
-//    document.getElementById('node1').setAttribute('checked',true);
 }
 
 
